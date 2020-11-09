f0d9fd0d641bd015807b2f44f64f38919ab40d46
khush7750 <meherkhushbant@gmail.com>
2020-11-10 00:18:27 +0530

    Updates

---
+++ precache-manifest.627168df450a6c5616f03668c8972598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1902f8771762d1f478f85ca2e7e716d",
+    "url": "/User-App/index.html"
+  },
+  {
+    "revision": "8a163e64278dd2cf9531",
+    "url": "/User-App/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "45a96e9dd30e475f2158",
+    "url": "/User-App/static/css/main.bcb5a572.chunk.css"
+  },
+  {
+    "revision": "8a163e64278dd2cf9531",
+    "url": "/User-App/static/js/2.9410bb71.chunk.js"
+  },
+  {
+    "revision": "7e9d5b8f94b8efb459854dc5316fe7df",
+    "url": "/User-App/static/js/2.9410bb71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a96e9dd30e475f2158",
+    "url": "/User-App/static/js/main.53c7279f.chunk.js"
+  },
+  {
+    "revision": "9b2fd0d772fc162190d5",
+    "url": "/User-App/static/js/runtime-main.32b76ab6.js"
+  }
+]);
